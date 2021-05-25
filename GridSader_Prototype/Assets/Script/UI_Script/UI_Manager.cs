d8b774cbd2f9321281367a7ff76dad95389ad4cd
Yesirsenic <yesirsenic@naver.com>
2021-05-25 20:00:39 +0900

    UI_manager making
    Ui_manager script make in script folder

--- GridSader_Prototype/Assets/Script/UI_Script/UI_Manager.cs
+++ GridSader_Prototype/Assets/Script/UI_Script/UI_Manager.cs
@@ -7,15 +7,6 @@ public class UI_Manager : MonoBehaviour
     // 싱글톤 패턴을 사용하기 위한 인스턴스 변수
     private static UI_Manager _instance;
 
-    //각각의 순서를 명시하는 변수
-    private int ui_order;
-
-    //순서 변수를 인트화시켜 보기 편화게 설정
-    enum orders
-    {
-        use_of_job_skills, character_stat_distribution, character_placement
-    }
-
     // 인스턴스에 접근하기 위한 프로퍼티
     private UI_Manager Instance
     {
@@ -46,6 +37,61 @@ public class UI_Manager : MonoBehaviour
         }
 
 
+
+    }
+
+    //순서 변수를 인트화시켜 보기 편화게 설정
+    enum orders
+    {
+        use_of_job_skills, character_stat_distribution, character_placement
+    }
+
+    //각각의 순서를 명시하는 변수
+    private orders ui_order;
+
+    
+
+    private void Start()
+    {
+        ui_order = orders.use_of_job_skills;// 일단 job skill 사용을 처음으로 설정
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void GetMOuseclick_Vector() //레이 케스트를 이용한 방법으로 마우스 클릭 좌표 흭득
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 Mouse_Position = Input.mousePosition;
+            
+
+        }
+    }
+
+
+    void Character_stat_distribution()// 캐릭터 스탯 분배 차례일때 함수 작동
+    {
+        if(ui_order== orders.character_stat_distribution)
+        {
+            //일단 밑에 캐릭터 인벤토리 등장은 이 스크립트가 아닌 오브젝트 내에 스크립트를 추가시켜 지금 순서가 캐릭터 스텟분배이면 곧바로 나타나도록 설정.
+
+            //인벤토리내 캐릭터 배치는 인벤토리의 크기를 설정하고 제작해야하므로 일단 잠시 보류.
+
+            //레이캐스트 제작이 끝났을시 캐릭터 클릭-> 팝업창 정보 등장하도록 설정
+            
+        }
+
+    }
+
+    void Character_placement()// 캐릭터 배치 단계일때 함수 작동
+    {
+        if(ui_order==orders.character_placement)
+        {
+
+        }
 
     }
 }
