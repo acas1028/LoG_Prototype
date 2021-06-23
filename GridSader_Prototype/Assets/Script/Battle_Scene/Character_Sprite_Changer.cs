4b84bd34e34871d1609a85bdce1cd63f469f8866
acas1028 <63082364+acas1028@users.noreply.github.com>
2021-06-23 15:35:07 +0900

    여러부분 fix
    1. 체력이 0이되면 사망하여
    묘비로 변함
    
    2. 메시지 UI 하단으로 이동
    
    3. layer변경
    (백그라운드 -> 그리드 -> 캐릭터 -> 피격 효과) 순으로 렌더링됨.

--- GridSader_Prototype/Assets/Script/Battle_Scene/Character_Sprite_Changer.cs
+++ GridSader_Prototype/Assets/Script/Battle_Scene/Character_Sprite_Changer.cs
@@ -21,7 +21,10 @@ public class Character_Sprite_Changer : MonoBehaviour
     public void Character_Sprite_Setting()
     {
         Character_ID = this.gameObject.GetComponent<Character_Script>().character_ID;
-        if (Character_ID < 1) return;
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = Character_Sprite[Character_ID - 1];
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = Character_Sprite[Character_ID];
+
+        if(this.gameObject.GetComponent<Character_Script>().character_Is_Allive == false)
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Character_Sprite[0];
+
     }
 }
