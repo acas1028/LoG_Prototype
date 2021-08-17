058ab998f8d167f641a2010b0ec6ad3fd533c560
sa04108 <sa04108@naver.com>
2021-08-17 15:44:14 +0900

    배치 과정 변경 (기획서에 맞게)
    배치씬에서 가운데에 별도로 놓여져있던 배치완료버튼을 삭제하고 기획서대로 준비버튼을 배치완료버튼으로 변경.
    룸에 두 플레이어 모두 입장시 0.5초간 선, 후공 결정 텍스트 출력 후 자동으로 선, 후공 결정.
    임시로 자기턴일때에만 준비버튼이 활성화되도록 했으나 차후 적절한 비활성화버튼 UI가 나오면 적용할 예정.

--- LoG/Assets/Script/Array_Script/ArrRoomManager.cs
+++ LoG/Assets/Script/Array_Script/ArrRoomManager.cs
@@ -27,49 +27,40 @@ public class ArrRoomManager : MonoBehaviourPunCallbacks
     public Text playerName;
     public Text roomStatusText;
     public Text joinedPlayerList;
-    public Text isEnemyReadyText;
+    public Text isEnemyJoinedText;
     public Text timeText;
     public Button readyButton;
-    public Button arrCompleteButton;
 
     Player firstPlayer;
     Player secondPlayer;
     bool isPreemptivePlayerSet;
 
-    private bool isReady;
-    private bool isEnemyReady;
-
     private void Start()
     {
         playerName.text = PhotonNetwork.LocalPlayer.NickName;
-
-        if (PhotonNetwork.OfflineMode)
-        {
-            roomStatusText.text = "<오프라인 모드>";
-            Hashtable player_1_table = new Hashtable() { { "IsPreemptive", true } };
-            PhotonNetwork.SetPlayerCustomProperties(player_1_table);
-        }
-        else if (!PhotonNetwork.IsConnected)
-            roomStatusText.text = "로그인이 필요합니다";
-        else
-            roomStatusText.text = " ";
-        isEnemyReadyText.text = " ";
+        isEnemyJoinedText.text = "상대의 입장을 기다리는 중입니다...";
         preemptiveCheck.text = " ";
 
         firstPlayer = PhotonNetwork.LocalPlayer;
         secondPlayer = PhotonNetwork.LocalPlayer;
         isPreemptivePlayerSet = false;
 
-        isReady = false;
-        isEnemyReady = false;
-
-        Hashtable isReady_table = new Hashtable();
-        isReady_table.Add("PlayerIsReady", false);
-        PhotonNetwork.LocalPlayer.SetCustomProperties(isReady_table);
-
         arrayPhase = (int)ArrayPhase.STANDBY;
 
         RenewPlayerList();
+
+        if (PhotonNetwork.OfflineMode)
+        {
+            roomStatusText.text = "<오프라인 모드>";
+            Hashtable player_1_table = new Hashtable() { { "IsPreemptive", true } };
+            PhotonNetwork.SetPlayerCustomProperties(player_1_table);
+            preemptiveCheck.text = "선공";
+            StartArrayPhase();
+        }
+        else if (!PhotonNetwork.IsConnected)
+            roomStatusText.text = "로그인이 필요합니다";
+        else
+            roomStatusText.text = " ";
     }
 
     #region 외부에서 호출되는 public 함수
@@ -88,47 +79,8 @@ public class ArrRoomManager : MonoBehaviourPunCallbacks
         return PhotonNetwork.CurrentRoom.PlayerCount >= 2;
     }
 
-    public void SetReady()
-    {
-        if (PhotonNetwork.OfflineMode)
-        {
-            StartArrayPhase();
-            return;
-        }
-
-        bool result = false;
-        isReady = !isReady;
-        SetReadyButtonStatus(isReady);
-
-        Hashtable isReady_table = new Hashtable();
-        isReady_table.Add("PlayerIsReady", isReady);
-
-        result = PhotonNetwork.LocalPlayer.SetCustomProperties(isReady_table);
-        if (!result)
-            Debug.LogWarning("IsReady Custom Property 설정 실패");
-    }
-
-    public void SetReadyButtonStatus(bool isReady)
-    {
-        ColorBlock cb = readyButton.colors;
-        cb.normalColor = new Color(isReady ? 1.0f : 0.0f, 0.0f, 0.0f);
-        cb.selectedColor = new Color(isReady ? 1.0f : 0.0f, 0.0f, 0.0f);
-        readyButton.colors = cb;
-        //readyButton.GetComponentInChildren<Text>().text = isReady ? "준비 완료!" : "준비!";
-    }
-
-    public void SetIsEnemyReadyText(bool isEnemyReady)
-    {
-        if (isEnemyReady)
-            isEnemyReadyText.text = "상대 준비 완료";
-        else
-            isEnemyReadyText.text = "상대 준비 미완료";
-    }
-
     public void SetPreemptivePlayer()
     {
-        Debug.Log("SetPreemptivePlayer() 호출");
-
         bool result = false;
         result = IsAllPlayersJoined();
         if (!result)
@@ -165,10 +117,8 @@ public class ArrRoomManager : MonoBehaviourPunCallbacks
     [PunRPC]
     private void NextArrayPhase()
     {
-        if (readyButton.gameObject.activeSelf)
-            readyButton.gameObject.SetActive(false);
-        if (isEnemyReadyText.gameObject.activeSelf)
-            isEnemyReadyText.gameObject.SetActive(false);
+        if (isEnemyJoinedText.gameObject.activeSelf)
+            isEnemyJoinedText.gameObject.SetActive(false);
         if (timeText.gameObject.activeSelf)
             timeText.gameObject.SetActive(false);
 
@@ -178,24 +128,24 @@ public class ArrRoomManager : MonoBehaviourPunCallbacks
         if (arrayPhase == (int)ArrayPhase.END)
         {
             roomStatusText.text = "모든 배치가 완료되었습니다.";
-            arrCompleteButton.gameObject.SetActive(false);
+            readyButton.gameObject.SetActive(false);
             PhotonNetwork.LoadLevel("BattleScene");
         }
         else if (arrayPhase % 2 == 0)
         {
             roomStatusText.text = "#" + (arrayPhase + 1) + " 선공 " + firstPlayer.NickName + ", " + (arrayPhase == (int)ArrayPhase.FIRST1 ? 1 : 2) + "개의 캐릭터를 배치하십시오.";
             if (IsPlayerPreemptive())
-                arrCompleteButton.gameObject.SetActive(true);
+                readyButton.gameObject.SetActive(true);
             else
-                arrCompleteButton.gameObject.SetActive(false);
+                readyButton.gameObject.SetActive(false);
         }
         else if (arrayPhase % 2 == 1)
         {
             roomStatusText.text = "#" + (arrayPhase + 1) + " 후공 " + secondPlayer.NickName + ", " + (arrayPhase == (int)ArrayPhase.SECOND5 ? 1 : 2) + "개의 캐릭터를 배치하십시오.";
             if (IsPlayerPreemptive())
-                arrCompleteButton.gameObject.SetActive(false);
+                readyButton.gameObject.SetActive(false);
             else
-                arrCompleteButton.gameObject.SetActive(true);
+                readyButton.gameObject.SetActive(true);
         }
 
         Debug.Log("<color=yellow>RPC Success with ArrayPhase: </color><color=lightblue>" + (ArrayPhase)arrayPhase + "</color>");
@@ -219,6 +169,9 @@ public class ArrRoomManager : MonoBehaviourPunCallbacks
         roomStatusText.text = "상대 플레이어 " + other.NickName + " 입장";
         Debug.Log("<color=yellow>플레이어 " + other.NickName + " 입장</color>");
         RenewPlayerList();
+
+        if (PhotonNetwork.IsMasterClient && IsAllPlayersJoined())
+            SetPreemptivePlayer();
     }
 
     public override void OnPlayerLeftRoom(Player other)
@@ -233,32 +186,8 @@ public class ArrRoomManager : MonoBehaviourPunCallbacks
         if (PhotonNetwork.OfflineMode)
             return;
 
-        // isReady_table 을 받아온 경우에만
-        if (changedProps.ContainsKey("PlayerIsReady"))
-        {
-            Debug.LogFormat("Player <color=lightblue>#{0} {1}</color> Properties Updated due to <color=green>{2}</color>", targetPlayer.ActorNumber, targetPlayer.NickName, changedProps.ToString());
-
-            // 상대가 먼저 방에 들어와서 준비버튼을 누른 경우에도 상대의 준비 여부를 받아올 수 있도록 한다.
-            foreach (var player in PhotonNetwork.PlayerListOthers)
-            {
-                object o_isEnemyReady;
-                player.CustomProperties.TryGetValue("PlayerIsReady", out o_isEnemyReady);
-                isEnemyReady = (bool)o_isEnemyReady;
-            }
-
-            SetIsEnemyReadyText(isEnemyReady);
-
-            bool isAllPlayerReady = isReady && isEnemyReady;
-
-            // 두 플레이어 준비 완료 후 배치 시작
-            if (PhotonNetwork.IsMasterClient && isAllPlayerReady)
-            {
-                SetPreemptivePlayer();
-                isReady = false;
-            }
-        }
         // 선, 후공 결정에 관한 데이터이면서 '나'의 선, 후공 데이터인 경우에만 아래 과정을 거친다.
-        else if (changedProps.ContainsKey("IsPreemptive") && targetPlayer == PhotonNetwork.LocalPlayer && !isPreemptivePlayerSet)
+        if (changedProps.ContainsKey("IsPreemptive") && targetPlayer == PhotonNetwork.LocalPlayer && !isPreemptivePlayerSet)
         {
             Debug.LogFormat("Player <color=lightblue>#{0} {1}</color> Properties Updated due to <color=green>{2}</color>", targetPlayer.ActorNumber, targetPlayer.NickName, changedProps.ToString());
 
@@ -285,9 +214,12 @@ public class ArrRoomManager : MonoBehaviourPunCallbacks
             }
 
             isPreemptivePlayerSet = true;
+            roomStatusText.text = "선, 후공을 결정합니다...";
 
             if (PhotonNetwork.IsMasterClient)
-                StartArrayPhase();
+            {
+                Invoke("StartArrayPhase", 1.0f);
+            }
         }
     }
     #endregion
