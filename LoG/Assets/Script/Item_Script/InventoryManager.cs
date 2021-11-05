c4fc888e2a50d6bd58fe738ab5e123df206dda52
sa04108 <sa04108@naver.com>
2021-11-05 21:50:37 +0900

    아이템 구매 기능 추가

--- LoG/Assets/Script/Item_Script/InventoryManager.cs
+++ LoG/Assets/Script/Item_Script/InventoryManager.cs
@@ -9,8 +9,12 @@ using PlayFab.ClientModels;
 public class InventoryManager : MonoBehaviour
 {
     private string playfabId;
+    private string lastAppleId;
+    private string lastPeachId;
 
     public Text gold;
+    public Text apple;
+    public Text peach;
 
     // Start is called before the first frame update
     void Start()
@@ -18,26 +22,78 @@ public class InventoryManager : MonoBehaviour
         playfabId = PlayerPrefs.GetString("PlayFabId");
         if (playfabId == string.Empty)
             Debug.LogError("로그인을 먼저 하십시오.");
+
+        gold.text = "0";
+        apple.text = "0";
+        peach.text = "0";
+
+        GetInventory();
     }
 
     public void GetInventory()
     {
         PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(), (result) =>
         {
-            print("현재 골드: " + result.VirtualCurrency["GD"]);
             gold.text = result.VirtualCurrency["GD"].ToString();
-        }, (error) => print("현재 골드 불러오기 실패"));
+            for (int i = 0; i < result.Inventory.Count; i++)
+            {
+                var inven = result.Inventory[i];
+                switch (inven.DisplayName)
+                {
+                    case "Apple":
+                        apple.text = inven.RemainingUses.ToString();
+                        lastAppleId = inven.ItemInstanceId;
+                        break;
+                    case "Peach":
+                        peach.text = inven.RemainingUses.ToString();
+                        lastPeachId = inven.ItemInstanceId;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }, (error) => print("인벤토리 불러오기 실패"));
     }
 
     public void AddGold()
     {
         var request = new AddUserVirtualCurrencyRequest() { VirtualCurrency = "GD", Amount = 100 };
-        PlayFabClientAPI.AddUserVirtualCurrency(request, (result) => { print("100 골드 획득"); gold.text = result.Balance.ToString(); }, (error) => print("골드 획득 실패"));
+        PlayFabClientAPI.AddUserVirtualCurrency(request, (result) => print("100 골드 획득"), (error) => print("골드 획득 실패"));
+        GetInventory();
     }
 
     public void SubtractGold()
     {
         var request = new SubtractUserVirtualCurrencyRequest() { VirtualCurrency = "GD", Amount = 100 };
-        PlayFabClientAPI.SubtractUserVirtualCurrency(request, (result) => { print("100 골드 차감"); gold.text = result.Balance.ToString(); }, (error) => print("골드 차감 실패"));
+        PlayFabClientAPI.SubtractUserVirtualCurrency(request, (result) => print("100 골드 차감"), (error) => print("골드 차감 실패"));
+        GetInventory();
+    }
+
+    public void BuyApple()
+    {
+        var request = new PurchaseItemRequest() { CatalogVersion = "Items", ItemId = "Apple", VirtualCurrency = "GD", Price = 500 };
+        PlayFabClientAPI.PurchaseItem(request, (result) => print("사과 구입"), (error) => print("사과 구입 실패"));
+        GetInventory();
+    }
+
+    public void BuyPeach()
+    {
+        var request = new PurchaseItemRequest() { CatalogVersion = "Items", ItemId = "Peach", VirtualCurrency = "GD", Price = 300 };
+        PlayFabClientAPI.PurchaseItem(request, (result) => print("복숭아 구입"), (error) => print("복숭아 구입 실패"));
+        GetInventory();
+    }
+
+    public void EatApple()
+    {
+        var request = new ConsumeItemRequest() { ConsumeCount = 1, ItemInstanceId = lastAppleId };
+        PlayFabClientAPI.ConsumeItem(request, (result) => print("사과를 먹었다."), (error) => print("사과를 먹지 못했다."));
+        GetInventory();
+    }
+
+    public void EatPeach()
+    {
+        var request = new ConsumeItemRequest() { ConsumeCount = 1, ItemInstanceId = lastPeachId };
+        PlayFabClientAPI.ConsumeItem(request, (result) => print("복숭아를 먹었다."), (error) => print("복숭아를 먹지 못했다."));
+        GetInventory();
     }
 }
