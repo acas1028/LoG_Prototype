1231467bec24c5040382d39f3432e19f6ab7c318
Yesirsenic <yesirsenic@naver.com>
2021-11-18 22:56:40 +0900

    배열 씬에서 스텟 보이도록 버그 수정
    배열 씬에서 나타난 인벤토리 버그 픽스 후 캐릭터 팝업에 그 캐릭터의 스텟이 표기되지 않았던 버그 수정

--- LoG/Assets/Script/UI_Script/Array_Scene/ShowingCharacterStats.cs
+++ LoG/Assets/Script/UI_Script/Array_Scene/ShowingCharacterStats.cs
@@ -7,8 +7,8 @@ public class ShowingCharacterStats : MonoBehaviour
 {
     public GameObject prefabCharacter;// 팝업창에 띄울 캐릭터
     public GameObject[] attack_Grid_Tile; //팝업창에 띄울 공격 범위
-    
-  
+
+
     public Text attack_Damage;// 팝업창에 띄울 캐릭터 공격력
     public Text health_Point;// 팝업창에 띄울 캐릭터 hp
     public Text property_text; //팝업창에 띄울 캐릭터 특성
@@ -21,14 +21,17 @@ public class ShowingCharacterStats : MonoBehaviour
     {
         lastPageNum = Deck_Data_Send.instance.lastPageNum;
         prefabCharacter.GetComponent<Character>().Character_Reset();
+        
     }
     
 
 
     public void Character_Showing_Stats(int inventory_Num) //캐릭터 스탯을 팝업창에 띄우기 위한 함수
     {
-     
-        //prefabCharacter.GetComponent<Character>().Copy_Character_Stat(Deck_Data_Send.instance.Save_Data[lastPageNum, inventory_Num-1]);
+        
+
+        prefabCharacter.GetComponent<Character>().Copy_Character_Stat(Deck_Data_Send.instance.Save_Data[lastPageNum, inventory_Num - 1]);
+        
         attack_Damage.text = prefabCharacter.GetComponent<Character>().character_Attack_Damage.ToString();
         health_Point.text = prefabCharacter.GetComponent<Character>().character_HP.ToString();
         property_text.text = this.GetComponent<Property_Name_To_Korean>().PropertyNameTOKorean(prefabCharacter.GetComponent<Character>().character_Skill.ToString());
