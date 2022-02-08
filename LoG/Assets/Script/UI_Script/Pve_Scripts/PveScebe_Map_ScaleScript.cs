1be60f15f87b4a41f04072b6db1313e0491f6b8a
Yesirsenic <yesirsenic@naver.com>
2022-02-08 17:15:24 +0900

    pve ui 이미지 해상도 문제 수정
    pve 배경이미지가 폰의 크기에 알맞지 않았던 것을 수정

--- LoG/Assets/Script/UI_Script/Pve_Scripts/PveScebe_Map_ScaleScript.cs
+++ LoG/Assets/Script/UI_Script/Pve_Scripts/PveScebe_Map_ScaleScript.cs
@@ -7,11 +7,14 @@ public class PveScebe_Map_ScaleScript : MonoBehaviour
     private Camera cam;
 
     private GameObject[] backGround;
+    private GameObject[] floor;
 
     void Start()
     {
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         backGround = GameObject.FindGameObjectsWithTag("BackGround");
+        floor = GameObject.FindGameObjectsWithTag("Pve_Floor");
+        
     }
 
     // Update is called once per frame
@@ -31,6 +34,14 @@ public class PveScebe_Map_ScaleScript : MonoBehaviour
             var vertRatio = backGround[i].GetComponent<SpriteRenderer>().bounds.size.y / 2;
 
             backGround[i].transform.localScale = new Vector3(horzExtent / horRatio, vertExtent / vertRatio, 1);
+            floor[i].transform.localScale = new Vector3(horzExtent / horRatio, 1.4f, 1f);
+
+            if(i != 0)
+            {
+                backGround[i].transform.position = new Vector3(backGround[i - 1].transform.position.x + horzExtent / horRatio / 2, 0, 1);
+
+                floor[i].transform.position = new Vector3(floor[i - 1].transform.position.x + horzExtent / horRatio / 2, 0, 1);
+            }
         }
 
 
