4878d2540518ba5669b1ee264506f077b6309746
Yesirsenic <yesirsenic@naver.com>
2022-02-21 15:41:59 +0900

    캐릭터 대기 모션 적용
    배치씬에서 캐릭터 대기 모션이 나타납니다.

---
+++ LoG/Assets/Script/UI_Script/Array_Scene/ArrayCharacterAnimation.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayCharacterAnimation : MonoBehaviour
+{
+    public GameObject[] myGrid;
+    public GameObject[] oppenentGrid;
+
+
+    private void Update()
+    {
+        ArrayIdleAnimation();
+    }
+
+    void ArrayIdleAnimation()
+    {
+        for(int i =0; i<myGrid.Length;i++)
+        {
+            if(myGrid[i].tag== "Character")
+            {
+                if(myGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().enabled==false)
+                {
+                    myGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().enabled = true;
+                }
+
+
+                if(myGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite== myGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[1])
+                {
+                    myGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 2);
+                }
+
+                if(myGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite == myGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[2])
+                { 
+                    myGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 3);
+                }
+
+                if (myGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite == myGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[3])
+                {
+                    myGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 1);
+                }
+
+                if (myGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite == myGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[4])
+                {
+                    myGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 0);
+                }
+            }
+
+        }
+
+        for (int i = 0; i < oppenentGrid.Length; i++)
+        {
+            if(oppenentGrid[i].tag== "Character")
+            {
+                if(oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().enabled==false)
+                {
+                    oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().enabled = true;
+                }
+
+                if (oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite == oppenentGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[1])
+                {
+                    oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 2);
+                }
+
+                if (oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite == oppenentGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[2])
+                {
+                    oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 3);
+                }
+
+                if (oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite == oppenentGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[3])
+                {
+                    oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 1);
+                }
+
+                if (oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<SpriteRenderer>().sprite == oppenentGrid[i].transform.Find("Character_Prefab").GetChild(0).GetComponent<CharacterSpriteManager>().characterSprites[4])
+                {
+                    oppenentGrid[i].transform.Find("Character_Prefab").GetComponent<Animator>().SetInteger("IdleNum", 0);
+                }
+            }
+        }
+    }
+}
