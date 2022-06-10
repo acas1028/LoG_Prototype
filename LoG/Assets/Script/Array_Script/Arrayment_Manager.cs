ec0995ecfc87127edf55199f70f1499c234d7838
kimyeonwan <dusdhks7@gmail.com>
2022-06-10 10:27:06 +0900

    Revert "PVP Inventory lock 관련 수정"
    This reverts commit b3d438a8c805657cc316dc60ed51bf60a6e15aa1.

--- LoG/Assets/Script/Array_Script/Arrayment_Manager.cs
+++ LoG/Assets/Script/Array_Script/Arrayment_Manager.cs
@@ -9,9 +9,8 @@ public class Arrayment_Manager : MonoBehaviourPun
 {
     #region
     private int lastPageNum;
-    private bool is_click_inventory = false;
     [SerializeField]
-    private bool[] b_ArrayedInven = new bool[7] { false,false,false,false,false,false,false };
+    private bool is_click_inventory = false;
     public bool Ready_Array = false; // 레디 버튼을 눌렀다.
     private bool my_turn = true; // 나의 턴이다.
     private bool Pick = true;// true라면 배치. false라면 정보를 읽을수만있다. 추가 배치를 막는다.
@@ -25,6 +24,8 @@ public class Arrayment_Manager : MonoBehaviourPun
     private int cancle_num;
     private int arrayedThisTurn;
     private int InvenNum;
+    [SerializeField]
+    private bool is_PopOn = false;
 
     [SerializeField]
     private List<int> invenNums;
@@ -99,10 +100,7 @@ public class Arrayment_Manager : MonoBehaviourPun
     {
         for (int i = 0; i < 7; i++)
         {
-            if(!b_ArrayedInven[i])
-            {
-                Inventory[i].GetComponent<Inventory_ID>().SetNotArrayed();
-            }
+            Inventory[i].GetComponent<Inventory_ID>().SetNotArrayed();
         }
     }
 
@@ -150,6 +148,9 @@ public class Arrayment_Manager : MonoBehaviourPun
         if (!hit)
             return;
 
+        if (is_PopOn == true)
+            return;
+
         if (hit.transform.tag == "Character") //자신이나 상대의 캐릭터를 클릭한 경우.
         {
             //PopUp_Manager.GetComponent<ShowingCharacterStats>().Character_Showing_Stats(hit.collider.gameObject.GetComponent<Character>().character_ID);
@@ -170,6 +171,7 @@ public class Arrayment_Manager : MonoBehaviourPun
                 Limit_Popup.Limit_Poup_instance.SetIsbutton(null);
                 Limit_Popup.Limit_Poup_instance.limit_Popup_button.SetActive(false);
             }
+
 
             for (int i = 0; i < 9; i++)
             {
@@ -218,7 +220,6 @@ public class Arrayment_Manager : MonoBehaviourPun
             }
 
             ArrayOnGrid(InvenNum, gridNum);
-            b_ArrayedInven[InvenNum] = true;
             is_click_inventory = false;
 
             Limit_Popup.Limit_Poup_instance.PopupOff();
@@ -787,5 +788,16 @@ public class Arrayment_Manager : MonoBehaviourPun
                 break;
         }
         return num;
+    }
+
+
+    public bool getisPopupOn()
+    {
+        return is_PopOn;
+    }
+
+    public void SetIsPopupOn(bool ispopupOn)
+    {
+        is_PopOn = ispopupOn;
     }
 }
